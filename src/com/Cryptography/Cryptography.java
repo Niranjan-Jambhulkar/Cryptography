38250db5cc294b5bb283a1c97132161e8757ba86
Niranjan <niranjan.jambhulkar@outlook.com>
2024-02-11 15:35:32 +0530

    Initial commit

--- src/com/Cryptography/Cryptography.java
+++ src/com/Cryptography/Cryptography.java
@@ -1,8 +1,9 @@
 package com.Cryptography;
-import java.util.Scanner;
+
 
 public class Cryptography {
     public static void main(String[] args) {
+
         System.out.println("Hello World");
     }
 }
