d74faf8127426933ba65a11e93e7370ff9be338a
Niranjan <niranjan.jambhulkar@outlook.com>
2024-02-12 13:49:03 +0530

    Update Cryptography.java

--- src/com/Cryptography/Cryptography.java
+++ src/com/Cryptography/Cryptography.java
@@ -1,9 +1,16 @@
 package com.Cryptography;
+import javax.swing.plaf.PanelUI;
+import java.util.Scanner;
 
-
+class Alphabet{
+//    String []alphabet = new String[26];
+    static {
+        String []alphabet = {"A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"};
+    }
+}
 public class Cryptography {
     public static void main(String[] args) {
-
+        Scanner sc = new Scanner(System.in);
         System.out.println("Hello World");
     }
 }
